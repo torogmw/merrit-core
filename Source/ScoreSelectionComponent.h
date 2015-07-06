38d53b1a8ed6c165bb20b5bc23497d2da3d57b13
Minwei Gu <minwei@spotify.com>
2015-07-05 23:16:38 -0400

    add first view

---
+++ Source/ScoreSelectionComponent.h
@@ -0,0 +1,90 @@
+/*
+  ==============================================================================
+
+  This is an automatically generated GUI class created by the Introjucer!
+
+  Be careful when adding custom code to these files, as only the code within
+  the "//[xyz]" and "//[/xyz]" sections will be retained when the file is loaded
+  and re-saved.
+
+  Created with Introjucer version: 3.1.0
+
+  ------------------------------------------------------------------------------
+
+  The Introjucer is part of the JUCE library - "Jules' Utility Class Extensions"
+  Copyright 2004-13 by Raw Material Software Ltd.
+
+  ==============================================================================
+*/
+
+#ifndef __JUCE_HEADER_71DDBC526DF13E06__
+#define __JUCE_HEADER_71DDBC526DF13E06__
+
+//[Headers]     -- You can add your own extra header files here --
+#include "JuceHeader.h"
+//[/Headers]
+
+
+
+//==============================================================================
+/**
+                                                                    //[Comments]
+    An auto-generated component, created by the Introjucer.
+
+    Describe your class and how it works here!
+                                                                    //[/Comments]
+*/
+class ScoreSelectionComponent  : public Component,
+                                 public ButtonListener
+{
+public:
+    //==============================================================================
+    ScoreSelectionComponent ();
+    ~ScoreSelectionComponent();
+
+    //==============================================================================
+    //[UserMethods]     -- You can add your own custom methods in this section.
+    //[/UserMethods]
+
+    void paint (Graphics& g);
+    void resized();
+    void buttonClicked (Button* buttonThatWasClicked);
+
+    // Binary resources:
+    static const char* violin_png;
+    static const int violin_pngSize;
+    static const char* guitar_png;
+    static const int guitar_pngSize;
+    static const char* piano_jpg;
+    static const int piano_jpgSize;
+
+
+private:
+    //[UserVariables]   -- You can add your own custom variables in this section.
+    //[/UserVariables]
+
+    //==============================================================================
+    ScopedPointer<ImageButton> violin;
+    ScopedPointer<Label> contentLabel;
+    ScopedPointer<ImageButton> guitar;
+    ScopedPointer<ImageButton> piano;
+    ScopedPointer<TextButton> sheet1;
+    ScopedPointer<TextButton> sheet2;
+    ScopedPointer<TextButton> sheet3;
+    ScopedPointer<Label> practiceLabel;
+    ScopedPointer<TextButton> sheet4;
+    ScopedPointer<TextButton> sheet5;
+    ScopedPointer<TextButton> sheet6;
+    ScopedPointer<TextButton> sheet7;
+    ScopedPointer<TextButton> sheet8;
+    ScopedPointer<TextButton> sheet9;
+
+
+    //==============================================================================
+    JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (ScoreSelectionComponent)
+};
+
+//[EndFile] You can add extra defines here...
+//[/EndFile]
+
+#endif   // __JUCE_HEADER_71DDBC526DF13E06__
